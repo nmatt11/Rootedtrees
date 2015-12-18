ba153c3dbf411eb9ca797d3dd635daab7b79919d
Nicky <Mattsson@mattsson-3.home>
2015-12-18 18:49:57 +0100

    Full description now added

--- rootedtrees.py
+++ rootedtrees.py
@@ -1,5 +1,10 @@
 """"
-rootedtrees is distributed in the hope that it will be useful, but
+rootedtrees, earlier planartrees, is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+rootedtrees, earlier planartrees, is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 Public License for more details.
